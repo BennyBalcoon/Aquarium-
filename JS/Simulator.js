0d6cfe1c1cef6ec9274ec02c23e8aa7dd4a75d8b
Benoit Balcon <benoit.b.balcon@renault.com>
2020-01-14 12:31:55 +0100

    cleaning simulation constructor

--- JS/Simulator.js
+++ JS/Simulator.js
@@ -1,40 +1,20 @@
 function Simulation() {
-  // this.aquarium;
-  // this.mouseX;
-  // this.mouseY;
+  // this.aquarium = aquarium;
 }
 
 Simulation.prototype.createLife = function() {
-  var myAquarium = window.document.createElement('div');
   var aquarium = new Aquarium();
-  myAquarium.style.backgroundColor = aquarium.color;
-  myAquarium.style.width = aquarium.width;
-  myAquarium.style.height = aquarium.height;
-  myAquarium.style.border = aquarium.border;
-  myAquarium.style.position = 'relative';
-
-  window.document.body.appendChild(myAquarium);
+  aquarium.drawAquarium();
 
   window.addEventListener('click', function(event) {
-    var nemo = window.document.createElement('div');
-    var fish = new Fish(event.pageX, event.pageY);
-
-    nemo.style.backgroundColor = fish.color;
-    nemo.style.width = fish.width;
-    nemo.style.height = fish.height;
-    nemo.style.top = fish.y - myAquarium.offsetTop + 'px';
-    nemo.style.left = fish.x - myAquarium.offsetLeft + 'px';
-    nemo.style.position = 'absolute';
-
-    var AquariumBorderLeft = event.pageX >= myAquarium.offsetLeft;
-    var AquariumBorderRight = event.pageX <= myAquarium.offsetLeft + 900;
-    var AquariumBorderTop = event.pageY >= myAquarium.offsetTop;
-    var AquariumBorderBottom = event.pageY <= myAquarium.offsetTop + 600;
-
-    console.log(aquarium);
-    console.log(myAquarium);
-    console.log(fish);
-    console.log(nemo);
+    // var nemo = window.document.createElement('div');
+    var nemo = new Fish(event.pageX, event.pageY, aquarium);
+    var AquariumBorderLeft = nemo.x >= aquarium.myAquarium.offsetLeft;
+    var AquariumBorderRight = nemo.x <= aquarium.myAquarium.offsetWidth;
+    var AquariumBorderTop = nemo.y >= aquarium.myAquarium.offsetTop;
+    var AquariumBorderBottom =
+      nemo.y <=
+      aquarium.myAquarium.clientHeight + aquarium.myAquarium.offsetTop;
 
     if (
       AquariumBorderBottom &&
@@ -42,10 +22,9 @@ Simulation.prototype.createLife = function() {
       AquariumBorderRight &&
       AquariumBorderTop
     ) {
-      aquarium.fishArray.push(fish);
-      // fish.aquarium = aquarium;
-      console.log(aquarium.fishArray);
-      myAquarium.appendChild(nemo);
+      aquarium.addFish(nemo);
+      nemo.drawFish();
+      nemo.move();
     }
   });
 };
