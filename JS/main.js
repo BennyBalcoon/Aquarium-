870aadd067e43bf89cc879ed4e1fc3052f44957f
Benoit Balcon <benoit.b.balcon@renault.com>
2020-01-14 12:32:23 +0100

    ideas commented

--- JS/main.js
+++ JS/main.js
@@ -2,3 +2,17 @@ window.addEventListener('load', function() {
   var goSimulation = new Simulation();
   goSimulation.createLife();
 });
+// 'use strict';
+
+// var Simuation = function() {
+//   this.aquarium = new Aquarium(this);
+// };
+
+// Simulation.prototype.start = function() {
+//   this.aquarium.drawAquarium();
+//   this.aquarium.addFishEvent();
+//   setInterval(this.aquarium.swimFish.bind(this.aquarium), 100);
+// };
+
+// var mySimulation = new Simulation();
+// mySimulation.start();
